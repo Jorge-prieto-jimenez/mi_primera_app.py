51fce4932f995c7c29333142c172a0ae93239e70
Jorge-prieto-jimenez <prietojorge905@gmail.com>
2024-03-05 22:27:28 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# Título y autor
+st.title("Mi primera app")
+st.write("Esta app fue elaborada por JORGE ANDRES PRIETO JIMENEZ")
+
+# Preguntar el nombre al usuario
+nombre_usuario = st.text_input("Por favor, ingresa tu nombre:")
+
+# Saludo personalizado
+if nombre_usuario:
+    st.write(f"{nombre_usuario}, te doy la bienvenida a mi primera app.")
